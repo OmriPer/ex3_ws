b4ad6ace22a06dac7e95f80d785c28f9aad71572
Shirel <shirelbb@gmail.com>
2025-12-22 20:06:57 +0000

    start align to target and move straight functions

--- controllers/controller_bug1.cpp
+++ controllers/controller_bug1.cpp
@@ -1,4 +1,5 @@
 #include "controller_bug1.hpp"
+#include <limits>
 
 namespace argos {
    
@@ -23,13 +24,98 @@ namespace argos {
       GetNodeAttribute(tTargetNode, "y", targetY);
       m_cTargetPosition.Set(targetX, targetY, 0.0);
 
-      /* your Init code here */
+      // Initialize Bug1 algorithm variables
+      m_eState = EBug1State::ALIGN_TO_TARGET;
+      m_fBestDist = std::numeric_limits<Real>::max();
+      m_bCompletedLoop = false;
+      m_cHitPoint.Set(0.0, 0.0);
+      m_cBestPoint.Set(0.0, 0.0);
    }
    
    void ControllerBug1::ControlStep() {
+
       /* your ControlStep code here */
+      switch(m_eState) {
+         case EBug1State::ALIGN_TO_TARGET:
+            AlignToTarget();
+            break;
+         case EBug1State::MOVE_STRAIGHT:
+            MoveStraight();
+            break;
+         // case EBug1State::FOLLOW_OBSTACLE:
+         //    break;
+         // case EBug1State::GO_TO_BEST_POINT:
+         //    break;
+         case EBug1State::FINISHED:
+            m_pcWheels->SetLinearVelocity(0.0, 0.0);
+            m_pcColoredLEDs->SetAllColors(CColor::GREEN);
+            break;
+      }
+   }
+   void ControllerBug1::AlignToTarget() {
+
+      m_pcColoredLEDs->SetAllColors(CColor::BLUE);
+
+      CVector3 currentPos = m_pcPositioning->GetReading().Position;
+      CVector2 cPos(currentPos.GetX(), currentPos.GetY());
+      CVector2 cTarget(m_cTargetPosition.GetX(), m_cTargetPosition.GetY());
+      CVector2 toTarget = cTarget - cPos;
+
+      CRadians cAngleError = toTarget.Angle();
+      cAngleError.SignedNormalize();
+
+      const Real fThreshold = 0.05; 
+
+      if(Abs(cAngleError.GetValue()) > fThreshold) {
+         m_pcWheels->SetLinearVelocity(-0.05, 0.05);
+      } else {
+         m_eState = EBug1State::MOVE_STRAIGHT;
+      }
+}
+
+   bool ControllerBug1::IsObstacleAhead(){
+      const Real OBSTACLE_THRESHOLD = 0.2; 
+
+      for(const auto& reading : m_pcRangefinders->GetReadings()) {
+         if(reading.Value < OBSTACLE_THRESHOLD) {
+            return true;
+         }
+      }
+      return false;
    }
 
+   void ControllerBug1::MoveStraight(){
+
+      m_pcColoredLEDs->SetAllColors(CColor::BLUE);
+
+      if(IsObstacleAhead()) {
+         CVector3 currentPos = m_pcPositioning->GetReading().Position;
+         m_cHitPoint.Set(currentPos.GetX(), currentPos.GetY()); // to remeber when a loop is completed
+
+         // initialize bug1 variables
+         CVector2 cTarget(m_cTargetPosition.GetX(), m_cTargetPosition.GetY());
+         CVector2 cPos(currentPos.GetX(), currentPos.GetY());
+
+         m_cBestPoint = cPos;
+         m_fBestDist = (cTarget - cPos).Length();
+         m_bCompletedLoop = false
+         // m_eState = EBug1State::FOLLOW_OBSTACLE;
+         return;
+      } else {
+      m_pcWheels->SetLinearVelocity(0.5, 0.5);
+
+   }
+}
+
+   void ControllerBug1::FollowObstacle(){
+
+      m_pcColoredLEDs->SetAllColors(CColor::YELLOW);
+     
+   }
+
+
+
+   /****************************************/
    /****************************************/
    /****************************************/
    
--- controllers/controller_bug1.hpp
+++ controllers/controller_bug1.hpp
@@ -6,6 +6,9 @@
 #include <argos3/plugins/robots/pi-puck/control_interface/ci_pipuck_system_sensor.h>
 #include <argos3/plugins/robots/pi-puck/control_interface/ci_pipuck_differential_drive_sensor.h>
 #include <argos3/plugins/robots/generic/control_interface/ci_positioning_sensor.h>
+#include <argos3/core/utility/math/vector3.h>
+#include <argos3/core/utility/math/vector2.h>
+#include <argos3/core/utility/math/radians.h>
 
 namespace argos {
 
@@ -31,5 +34,25 @@ namespace argos {
       CCI_PiPuckSystemSensor* m_pcSystem = nullptr;
       CCI_PositioningSensor* m_pcPositioning = nullptr;
       CVector3 m_cTargetPosition;
-   };
+
+      /* Bug1 algorithm variables */
+      enum class EBug1State {
+         ALIGN_TO_TARGET,
+         MOVE_STRAIGHT,
+         // FOLLOW_OBSTACLE,
+         // GO_TO_BEST_POINT,
+         FINISHED
+      };
+      EBug1State m_eState; 
+      CVector2 m_cHitPoint; // Point where the robot first encountered the obstacle
+      CVector2 m_cBestPoint; // Closest point to the target found while following the obstacle
+      Real m_fBestDist; // Distance to the target at the best point
+      bool m_bCompletedLoop; // Flag to indicate if a full loop around the obstacle has been completed
+
+      void GoToTarget();
+      bool IsObstacleAhead();
+      void MoveTowards(const CVector2& cDir);
+
+
+};
 }
